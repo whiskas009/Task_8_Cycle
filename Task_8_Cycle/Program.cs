4b682bd3b73c86f680e68609902689f529257a43
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-05-12 21:45:06 +0300

    #1

---
+++ Task_8_Cycle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_8_Cycle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите фразу");
+            string phrase = Console.ReadLine();
+
+            Console.WriteLine("Введите кол-во повторений");
+            int numberRepetitionsPhrase = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberRepetitionsPhrase; i++)
+            {
+                Console.WriteLine(phrase);
+            }
+        }
+    }
+}
